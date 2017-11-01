b48c33e64d13e17ea24748bb6727c52c6cb71fd7
Lars Wallenborn <lars@wallenborn.net>
2017-11-01 17:31:22 +0100

    updates lib

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request, abort, g
-from lib.data import SampleFactory
+from lib.sample import SampleFactory
 import string
 import os
 import logging
@@ -38,37 +38,24 @@ def close_db(error):
         g.db.close()
 
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
-        'done': False
-    }
-]
-supported = ["test"]
+supported = ['test']
 
 
 @app.route('/', methods=['POST'])
 def get_task():
-    selected_plugin = ""
+    selected_plugin = ''
     if not request.json or not 'plugins' in request.json:
         abort(400)
-    if len(request.json.get("plugins")) == 0:
+    if len(request.json.get('plugins')) == 0:
         abort(400)
-    for plugin in request.json.get("plugins"):
+    for plugin in request.json.get('plugins'):
         if plugin in supported:
             selected_plugin = plugin
             break
     else:
         abort(400)
-    return jsonify({'tasks': tasks})
+    task = []
+    return jsonify({'task': task})
 
 
 def ensure_row(conn, table, field, value):
